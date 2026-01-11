6caf608aba35a5d1243c7681101f1c46346c6f63
Srushti <ksrushti1610@gmail.com>
2026-01-11 18:16:41 +0530

    Rename users table to auth_users and add ratings table

--- database.py
+++ database.py
@@ -4,8 +4,9 @@ conn = sqlite3.connect("app.db", check_same_thread=False)
 cursor = conn.cursor()
 
 def init_db():
+
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS auth_users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         email TEXT UNIQUE,
@@ -16,9 +17,20 @@ def init_db():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS profiles (
         user_id INTEGER,
+        role TEXT,
         grade TEXT,
-        subject TEXT,
-        availability TEXT
+        class INTEGER,
+        time TEXT,
+        strong_subjects TEXT,
+        weak_subjects TEXT,
+        teaches TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS ratings (
+        mentor TEXT,
+        rating INTEGER
     )
     """)
 
